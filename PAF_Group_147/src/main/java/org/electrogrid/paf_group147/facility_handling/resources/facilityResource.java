3eb4fb1fbe4a2b71385bab2662dbb1b67f7cff07
IT20255756 <79696488+IT20255756@users.noreply.github.com>
2022-04-17 19:59:03 +0530

    Update project

--- PAF_Group_147/src/main/java/org/electrogrid/paf_group147/facility_handling/resources/facilityResource.java
+++
@@ -1,5 +0,0 @@
-package org.electrogrid.paf_group147.facility_handling.resources;
-
-public class facilityResource {
-
-}
--- PAF_Group_147/src/main/java/org/electrogrid/paf_group147/facility_handling/service/facilityService.java
+++
@@ -1,5 +0,0 @@
-package org.electrogrid.paf_group147.facility_handling.service;
-
-public class facilityService {
-
-}
